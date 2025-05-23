8c8b6e81b710c5cdc97bfe6581cee141b2a67832
Rafael Andrade <andraderafael22@hotmail.com>
2025-05-23 20:01:33 -0300

    setup: env vars; database; docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Transaction } from './entities/transaction.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Transaction],
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([User, Transaction]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
