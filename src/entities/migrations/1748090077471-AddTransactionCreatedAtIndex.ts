875206da59dc9dce2c51203b78fe76899a1fa95a
Rafael Andrade <andraderafael22@hotmail.com>
2025-05-24 10:35:11 -0300

    fix: move migrations to correct folder and index creation

---
+++ src/entities/migrations/1748090077471-AddTransactionCreatedAtIndex.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class AddTransactionCreatedAtIndex1748090077471 implements MigrationInterface {
+  name = 'AddTransactionCreatedAtIndex1748090077471'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE INDEX "IDX_e744417ceb0b530285c08f3865" ON "financial"."transactions" ("createdAt") `,
+    )
+    await queryRunner.query(
+      `CREATE INDEX "IDX_f4bab09efce4a0e46784d0ccf4" ON "financial"."transactions" ("type", "createdAt") `,
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "financial"."IDX_f4bab09efce4a0e46784d0ccf4"`)
+    await queryRunner.query(`DROP INDEX "financial"."IDX_e744417ceb0b530285c08f3865"`)
+  }
+}
